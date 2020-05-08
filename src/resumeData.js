e8433a7ee15d8fd8badbe0a998fc9fafe6e84c92
Marcelo <teixeira@eida.lu>
2020-05-08 19:49:39 +0200

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,190 @@
+let resumeData = {
+    "imagebaseurl": "https://marcelo.xepanunty.com/",
+    "name": "Marcelo Teixeira",
+    "role": "CTO / Senior Full Stack / Project Manager",
+    "linkedinId": "andremarcelo",
+    "skypeid": "marcelo.teixeirab",
+    "roleDescription": "Now new adventures...\n" +
+        "Currently, I’m available to support/help companies to improve internal processes (Supply chain/Others)\n" +
+        "I can accept any project in consulting (freelancer/other types (speaking!)).\n" +
+        "In Energy, banking, trading, transports, or specific business.",
+    "socialLinks": [
+        {
+            "name": "linkedin",
+            "url": "https://www.linkedin.com/in/andremarcelo/",
+            "className": "fa fa-linkedin"
+        },
+        {
+            "name": "github",
+            "url": "https://github.com/andremarcelo",
+            "className": "fa fa-github"
+        }
+    ],
+    "aboutme": "I am currently CTO/ Full Stack Developer / Manager. Right now i search one motivated project. " +
+        "Currently, I’m available to support/help companies to improve internal processes (Supply chain/Others)\n" +
+        "I can accept any project in consulting (freelancer/other types (speaking!)).\n" +
+        "In Energy, banking, trading, transports, or specific business. I'm PHP frameworks expert + Databases. Web apps and invoice systems is my expertise!",
+    "address": "Luxembourg",
+    "website": "https://marcelo.xepanunty.com",
+    "education": [
+        {
+            "UniversityName": "IPP - Instituto politecnico do Porto, Porto",
+            "specialization": "Bachelor Degree",
+            "MonthOfPassing": "09",
+            "YearOfPassing": "2011",
+            "Achievements": "COMPUTER SCIENCE : Computer Programming, Computer & Information Sciences, Management Information Systems\n" +
+                "informatic computer science computer engenharia and networks information systems.\n" +
+                "Master Degree in Artificial Intelligence not completed!"
+        },
+        {
+            "UniversityName": "IPP - Instituto politecnico do Porto, Porto",
+            "specialization": "Master Degree",
+            "MonthOfPassing": "09",
+            "YearOfPassing": "2013",
+            "Achievements": "Artificial Intelligence : not completed : final project incompleted"
+        }
+    ],
+    "work": [
+        {
+            "CompanyName": "Eida Sa, Luxembourg (Luxembourg)",
+            "specialization": "CTO & SENIOR FULL-STACK DEVELOPER",
+            "MonthOfLeaving": "09/2017 to ",
+            "YearOfLeaving": "04/2020",
+            "Achievements": "Developed :\n" +
+                "▪ - DB Core (postgres + mysql)\n" +
+                "▪ - EDIFACT system convert EDI to JSON\n" +
+                "▪ - Core of energy messages\n" +
+                "▪ - Internal Backend\n" +
+                "▪ - Invoice Systems\n" +
+                "▪ - Client portal in angular with API and node.js" +
+                "▪ MacoLux Implementation - CREOS market Comunication\n" +
+                "▪ Teach the Team.\n" +
+                "▪ PHP 7, AWS tecnologies RDS(mariaDB).\n" +
+                "▪ Backend : backoffice internal : node js + PHP\n" +
+                "▪ Frontend : Portal Client : angular 6-8\n" +
+                "▪ Energy concepts and sector\n" +
+                "▪ POSTGRES & MySql Databases"
+        },
+        {
+            "CompanyName": "growney GmbH, Berlin (Germany)",
+            "specialization": "SENIOR FULL-STACK DEVELOPER",
+            "MonthOfLeaving": "07/2016 to",
+            "YearOfLeaving": "09/2017",
+            "Achievements": "▪ In this company i migrate one platform for the internal company.\n" +
+                "▪ The code based is created in PHP yii framework and connected to several banks.\n" +
+                "▪ One i arrive in the company i migrate the code to the AWS ec2 instances, and automatize the\n" +
+                "deploy process, using bit-bucket pipelines, and S3 buckets.\n" +
+                "▪ I improve 90% of the code, and create new features, inclusive i create one part integrated to\n" +
+                "angularjs, integrate angularjs to yii2.\n" +
+                "▪ Migrate the Mysql database to RDS, and improve the datebase structure.\n" +
+                "▪ Integtrate serveral services, Postidentity API, and others."
+        },
+        {
+            "CompanyName": "Choice Technologies sarl, Luxembourg (Luxembourg)",
+            "specialization": "Freelance - Support TEAM",
+            "MonthOfLeaving": "06/2016 to ",
+            "YearOfLeaving": "07/2016",
+            "Achievements": "Improved Platform, related to invoicing using angular js"
+        },
+        {
+            "CompanyName": "Agora+, Porto (Portugal)",
+            "specialization": "Senior Developer - Freelance and Support",
+            "MonthOfLeaving": "03/2016 to ",
+            "YearOfLeaving": "06/2016",
+            "Achievements": "Create one application connected to oracle service using APEX web-service."
+        },
+        {
+            "CompanyName": "Agap2 Consulting, Lisbon (Portugal)",
+            "specialization": "Senior Developer - consultant",
+            "MonthOfLeaving": "10/2015 to",
+            "YearOfLeaving": "03/2016",
+            "Achievements": "I work for several companies, insurance, and Banking."
+        },
+        {
+            "CompanyName": "Rocket Internet Portugal Rebrand in 2014 to Jumia Porto Tech Center, Porto (Portugal)",
+            "specialization": "PHP Developer(regular, advanced, senior, mobile APIs Manager)",
+            "MonthOfLeaving": "03/2012 to",
+            "YearOfLeaving": "10/2015",
+            "Achievements": "▪ Tecnologies backgorund : PHP, CSS, Javascript, Jquery, SaaS, Yii framework 1, Zend Framework\n" +
+                "1-2, Rest APis, Redis, others queues and search systems.\n" +
+                "▪ Developer one API rest for the mobile Aplications.\n" +
+                "▪ Projects intern i work : OfficeYes, Zalando,Kaymu. Mobile Team.\n" +
+                "▪ Visit Berlin and work from berlin sometimes.\n" +
+                "▪ Main Language : PHP"
+        },
+        {
+            "CompanyName": "Banco Espirito Santo, Lisbon (Portugal)",
+            "specialization": "Regular Developer - Java Developer",
+            "MonthOfLeaving": "09/2011 to ",
+            "YearOfLeaving": "03/2012",
+            "Achievements": "I work for several companies, insurance, and Banking."
+        },
+    ],
+    "skillsDescription": "Your skills here",
+    "skills": [
+        {
+            "skillname": "SAP"
+        },
+        {
+            "skillname": "PHP FRAMEWORKS"
+        },
+        {
+            "skillname": "MYSQL"
+        },
+        {
+            "skillname": "DATABASES IN GLOBAL"
+        },
+        {
+            "skillname": "FRONTEND : REACT ANGULAR"
+        },
+        {
+            "skillname": "EDIFACT"
+        },
+        {
+            "skillname": "APIS"
+        },
+        {
+            "skillname": "MOBILE"
+        },
+        {
+            "skillname": "HTML5"
+        },
+    ],
+    "portfolio": [
+        {
+            "name": "EDIFACT EIDA  : MAKET COMMUNICATION",
+            "description": "market communication in eida",
+            "imgurl": "https://mteixeira-site.s3.eu-central-1.amazonaws.com/bots.png"
+        },
+        {
+            "name": "Backoffice",
+            "description": "Internal Plataform",
+            "imgurl": "https://mteixeira-site.s3.eu-central-1.amazonaws.com/backoffice.png"
+        },
+        {
+            "name": "Invoices",
+            "description": "Energie invoices : Electricity and Gas",
+            "imgurl": "https://mteixeira-site.s3.eu-central-1.amazonaws.com/invoice.png"
+        },
+        {
+            "name": "my-eida",
+            "description": "My Eida portal",
+            "imgurl": "https://mteixeira-site.s3.eu-central-1.amazonaws.com/my.eida.jpg"
+        }
+    ],
+    "testimonials": [
+        {
+            "description": "André Marcelo Teixeira, was working for our company from 08/2017 until 04/2020. During\n" +
+                "this time he did an excellent job. He’s mission was to create an entirely new backoffice IT-\n" +
+                "platform integrating the invoicing, customer database and a dedicated customer portal."+
+                "Marcelo succeeded in achieving the fixed objectives",
+            "name": "EIDA SA : CEO"
+        },
+        {
+            "description": "Marcelo has long years of experience in development of complex and not-trivial applications, experience that has strong evidence in his work, as he's a hard worker, dedicated, get things done, always takes initiatives and a very knowledgeable developer that is able to pick up new technologies extremely fast.",
+            "name": "GROWNEY CEO"
+        }
+    ]
+}
+
+export default resumeData
